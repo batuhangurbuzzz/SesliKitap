cf9e7cc70179a50cd1dfaa933597e624aa60bd07
Batuhan GÜRBÜZ <batuhang002@gmail.com>
2025-05-03 12:07:11 +0300

    project finish

--- main .py
+++ main .py
@@ -3,9 +3,7 @@ from gtts import gTTS
 import os
 import tkinter as tk
 from tkinter import filedialog
-
-
-
+# Functions
 def extractPDFText(pdfFile):
     text = ""
     
@@ -14,15 +12,12 @@ def extractPDFText(pdfFile):
     for pageNum in range(len(pdfReader.pages)):
         text += pdfReader.pages[pageNum].extract_text()
     return text
-    
-    
-    
+     
     
 def turnIntoSound(text, soundFile):
     soundTranslate = gTTS(text=text, lang='tr')
     soundTranslate.save(soundFile)
-    
-    
+      
 
 def fileSelect():
     filePath = filedialog.askopenfilename(filetypes=[("PDF Dosyaları", "*pdf")])
@@ -33,5 +28,18 @@ def fileSelect():
         
         os.system("start kaydet.mp3")
         
-        
+# Design
+app = tk.Tk()
+app.title("Sesli Kitap Uygulaması")
+app.geometry("250x150")
+
+
+button = tk.Button(app, text="PDF Seç", command=fileSelect, padx=20, pady=20)
+
+button.pack(pady=20)
+
+
+
+
+app.mainloop()
         
